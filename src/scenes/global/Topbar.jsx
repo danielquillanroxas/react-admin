76421fa91217d7c641c05f0c546e2f84c0c6f984
danielquillanroxas <casrox54@yahoo.com>
2023-02-12 06:58:03 +0300

    updates

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,5 +1,24 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext } from "../../theme";
+import InputBase from "@mui/material";
+import LightModeOutlined from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlined from "@mui/icons-material/LightModeOutlined";
+import NotificationOutlined from "@mui/icons-material/LightModeOutlined";
+import SettingsOutlined from "@mui/icons-material/LightModeOutlined";
+import PersonOutlined from "@mui/icons-material/LightModeOutlined";
+import SearchIcon from "@mui/icons-material/LightModeOutlined";
+
+
 const Topbar = () => {
-    return <div>Topbar</div>
-}
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const colorMode = useContext(ColorModeContext);
+
+    return (<Box display="flex" justifyContent="space-between" p={2}>
+
+        <IconButton sx={ display:flex }></IconButton>
+    </Box>); 
+};
 
 export default Topbar;
