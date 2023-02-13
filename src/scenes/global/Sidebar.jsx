70419d62aa46a8f9bf06ab07a95f8fb4dbafc49d
danielquillanroxas <casrox54@yahoo.com>
2023-02-13 06:24:37 +0300

    finished sidebar

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -102,7 +102,7 @@ const Sidebar = () => {
 
                                 <Box textAlign="center">
                                     <Typography variant="h2" color={colors.grey[100]} fontWeight="bold" sx={{m: "10px 0 0 0"}}>Ed Roh</Typography>
-                                    <Typography variant="h5" color={colors.greenAccent[500]}>VP Fancy Admin</Typography>
+                                    <Typography variant="h5" color={colors.greenAccent[500]}>VPS Fancy Admin</Typography>
                                 </Box>
                             </Box>
                         )}
@@ -116,9 +116,105 @@ const Sidebar = () => {
                             setSelected={setSelected}
                             />
 
-                            <Typography>
-                                
+                            <Typography
+                            variant="h6"
+                            color={colors.grey[300]}
+                            sx={{m: "15px 0 5px 20px" }}
+                            >
+                                Data
                             </Typography>
+                            <Item 
+                            title="Manage Team"
+                            to="/team"
+                            icon={<PeopleOutlinedIcon/>}
+                            selected={selected}
+                            setSelected={setSelected}
+                            />
+                            <Item
+                            title="Contacts Information"
+                            to="/contacts"
+                            icon={<ContactsOutlinedIcon/>}
+                            selected={selected}
+                            setSelected={setSelected}
+                            />
+                            <Item
+                            title="Invoices Balances"
+                            to="/invoices"
+                            icon={<ReceiptOutlinedIcon/>}
+                            selected={selected}
+                            setSelected={setSelected}
+                            />
+                            <Typography
+                            variant="h6"
+                            color={colors.grey[300]}
+                            sx={{m:"15px 0 5px 20px"}}
+                            >
+                                Pages
+                            </Typography>
+
+                            <Item
+                            title="Profile Form"
+                            to="/form"
+                            icon={<PersonOutlinedIcon/>}
+                            selected={selected}
+                            setSelected={setSelected}
+                            />
+                            <Item
+                            title="Calendar"
+                            to="/calendar"
+                            icon={<CalendarTodayOutlinedIcon/>}
+                            selected={selected}
+                            setSelected={setSelected}
+                            />
+                             <Item
+                            title="FAQ Page"
+                            to="/faq"
+                            icon={<HelpOutlinedIcon/>}
+                            selected={selected}
+                            setSelected={setSelected}
+                            />
+
+                            <Typography
+                            variant="h6"
+                            color={colors.grey[300]}
+                            sx={{ m: "15px 0 5px 20px" }}
+                            >
+                                Charts
+                            </Typography>
+                            
+                            <Item
+                            title="Bar Chart"
+                            to="/bar"
+                            icon={<BarChartOutlinedIcon/>}
+                            selected={selected}
+                            setSelected={setSelected}
+                            />
+                            
+                            <Item
+                            title="Line Chart"
+                            to="/line"
+                            icon={<TimelineOutlinedIcon/>}
+                            selected={selected}
+                            setSelected={setSelected}
+                            />
+
+                            <Item
+                            title="Pie Chart"
+                            to="/pie"
+                            icon={<PieChartOutlineOutlinedIcon/>}
+                            selected={selected}
+                            setSelected={setSelected}
+                            />
+
+                            <Item
+                            title="Geography Chart"
+                            to="/geography"
+                            icon={<MapOutlinedIcon/>}
+                            selected={selected}
+                            setSelected={setSelected}
+                            />
+                            
+
 
 
                         </Box>
