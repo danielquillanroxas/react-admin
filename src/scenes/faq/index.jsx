f1282643a756896142b48fb9742f796b68317d4d
danielquillanroxas <casrox54@yahoo.com>
2023-02-13 17:18:32 +0300

    new

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -3,9 +3,18 @@ import Header from "../../components/Header";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { tokens } from "../../theme";
+import { palette } from "@mui/system";
 
 const FAQ = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
 
+    return <Box m="20px" display="flex" alignContent="center">
+        <Header title="FAQ" subtitle="Frequently Asked Questions Page"/>
+    </Box>
 }
+
 
 export default FAQ;
