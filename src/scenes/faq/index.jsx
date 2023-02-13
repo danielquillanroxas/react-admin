7d2f74c2450717a1b12bd4355a3e3bd97974fd87
danielquillanroxas <casrox54@yahoo.com>
2023-02-13 17:34:55 +0300

    FAQ page done

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -1,4 +1,4 @@
-import { Box, useTheme } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
 import Header from "../../components/Header";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
@@ -11,8 +11,73 @@ const FAQ = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
 
-    return <Box m="20px" display="flex" alignContent="center">
+    return <Box m="20px">
         <Header title="FAQ" subtitle="Frequently Asked Questions Page"/>
+
+        <Accordion> 
+            <AccordionSummary expandIcon = {<ExpandMoreIcon/>}>
+                <Typography color={colors.greenAccent[500]} variant="h5">
+                    An Important Question 
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Aute adipisicing labore nulla sint fugiat pariatur aliquip excepteur Lorem voluptate velit. Est culpa mollit esse labore. Ullamco in labore anim consectetur irure nulla. Excepteur in duis aliqua aute anim magna anim pariatur dolor minim.
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+
+        <Accordion> 
+            <AccordionSummary expandIcon = {<ExpandMoreIcon/>}>
+                <Typography color={colors.greenAccent[500]} variant="h5">
+                    An Important Question 
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Aute adipisicing labore nulla sint fugiat pariatur aliquip excepteur Lorem voluptate velit. Est culpa mollit esse labore. Ullamco in labore anim consectetur irure nulla. Excepteur in duis aliqua aute anim magna anim pariatur dolor minim.
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+
+        <Accordion> 
+            <AccordionSummary expandIcon = {<ExpandMoreIcon/>}>
+                <Typography color={colors.greenAccent[500]} variant="h5">
+                    An Important Question 
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Aute adipisicing labore nulla sint fugiat pariatur aliquip excepteur Lorem voluptate velit. Est culpa mollit esse labore. Ullamco in labore anim consectetur irure nulla. Excepteur in duis aliqua aute anim magna anim pariatur dolor minim.
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+
+        <Accordion> 
+            <AccordionSummary expandIcon = {<ExpandMoreIcon/>}>
+                <Typography color={colors.greenAccent[500]} variant="h5">
+                    An Important Question 
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Aute adipisicing labore nulla sint fugiat pariatur aliquip excepteur Lorem voluptate velit. Est culpa mollit esse labore. Ullamco in labore anim consectetur irure nulla. Excepteur in duis aliqua aute anim magna anim pariatur dolor minim.
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+
+        <Accordion> 
+            <AccordionSummary expandIcon = {<ExpandMoreIcon/>}>
+                <Typography color={colors.greenAccent[500]} variant="h5">
+                    An Important Question 
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Aute adipisicing labore nulla sint fugiat pariatur aliquip excepteur Lorem voluptate velit. Est culpa mollit esse labore. Ullamco in labore anim consectetur irure nulla. Excepteur in duis aliqua aute anim magna anim pariatur dolor minim.
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
     </Box>
 }
 
