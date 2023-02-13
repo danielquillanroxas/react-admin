fa37c3a53823f2b90a7270cfdf8f263fa40f9332
danielquillanroxas <casrox54@yahoo.com>
2023-02-13 17:07:38 +0300

    FAQ

---
+++ src/scenes/faq/index.jsx
@@ -0,0 +1,11 @@
+import { Box, useTheme } from "@mui/material";
+import Header from "../../components/Header";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+
+const FAQ = () => {
+
+}
+
+export default FAQ;
