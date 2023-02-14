6e60a6901e328a7a32fe0a5cb6c1a6000260b5f5
danielquillanroxas <casrox54@yahoo.com>
2023-02-14 08:18:01 +0300

    progress circle & StatBox done

---
+++ src/components/StatBox.jsx
@@ -0,0 +1,24 @@
+import { Box, useTheme, Typography } from "@mui/material";
+import { tokens } from "../theme";
+import ProgressCircle from "./ProgressCircle";
+
+const StatBox = ({ title, subtitle, icon, progress, increase }) => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
+    return (
+        <Box width="100%" m="0 30px">
+            <Box display="flex" justifyContent="space-between">
+                <Box>
+                    {icon}
+                    <Typography variant = "h4" fontWeight="bold" sx={{ color: colors.grey }}>
+                        {title}
+                    </Typography>
+                </Box>
+                <Box>
+                    <ProgressCircle progress={progress} />
+                </Box>
+            </Box>
+        </Box>
+    )
+}
