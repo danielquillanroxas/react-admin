d89b5556e3ff50c8c1c19fac8d9541c445d4b818
danielquillanroxas <casrox54@yahoo.com>
2023-02-14 09:20:32 +0300

    dashboard

--- src/components/StatBox.jsx
+++ src/components/StatBox.jsx
@@ -11,14 +11,25 @@ const StatBox = ({ title, subtitle, icon, progress, increase }) => {
             <Box display="flex" justifyContent="space-between">
                 <Box>
                     {icon}
-                    <Typography variant = "h4" fontWeight="bold" sx={{ color: colors.grey }}>
+                    <Typography variant = "h4" fontWeight="bold" sx={{ color: colors.grey[100] }}>
                         {title}
                     </Typography>
                 </Box>
                 <Box>
                     <ProgressCircle progress={progress} />
                 </Box>
+                <Box display="flex" justifyContent="space-between">
+                <Typography variant = "h5" sx={{ color: colors.greenAccent[500] }}>
+                        {subtitle}
+                </Typography>
+                <Typography variant = "h5" fontStyle="italic" sx={{ color: colors.greenAccent[600] }}>
+                        {increase}
+                </Typography>
+                    
+                </Box>
             </Box>
         </Box>
-    )
-}
+    );
+};
+
+export default StatBox;
