08637338e734d8b6f77d47147c5efc660c78014b
danielquillanroxas <casrox54@yahoo.com>
2023-02-14 07:38:50 +0300

    geo chart done

---
+++ src/scenes/geography/index.jsx
@@ -0,0 +1,20 @@
+import { Box, useTheme } from "@mui/material";
+import Header from "../../components/Header";
+import { tokens } from "../../theme";
+import GeographyChart from "../../components/GeographyChart";
+ 
+const Geography = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    return (
+        <Box m="20px">
+            <Header title="Geography Chart" subtitle="Simple Geogpraphy Chart"  border={`1px solid ${colors.grey[100]}`}
+        borderRadius="4px"/>
+            <Box height="75vh">
+                <GeographyChart />
+            </Box>
+        </Box>
+    );
+};
+
+export default Geography;
